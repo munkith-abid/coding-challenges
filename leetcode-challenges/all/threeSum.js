bd0f1b5153e2c8172ba06f77a1645055fc02f8ef
Mnkith <munkithabid@gmail.com>
2021-11-10 19:08:54 -0600

    formulize a solution

---
+++ leetcode-challenges/all/threeSum.js
@@ -0,0 +1,26 @@
+function threeSum(nums) {
+  
+  nums.sort();
+  let i = nums.lastIndexOf(0);
+  const firstPositiveIndex = i === -1 ? nums.findIndex(n => n > 0) : i;
+  let k;
+  
+  let res = [];
+  for (let i = 0; nums[i] <= 0; i++) {
+    let kPos = nums.length - 1
+    for (let j = i + 1; j < nums.length; j++) {
+      k  = j + 1;
+      k = j > firstPositiveIndex ? j + 1 : firstPositiveIndex;
+      for(; k < nums.length; k++) {
+        counter++;
+        if ( nums[j] < 0 && ( nums[k] < 0  ) )
+          continue;
+        if ((i !== j && j !== k && i !== k) && (nums[j]) === -(nums[i] + nums[k])) {
+          
+          
+        }
+      }
+    }
+  }
+  return res;
+}
