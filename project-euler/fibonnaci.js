0d1c1c43fe9fa83346b862fb5856473008cef4c1
munkith-abid <munkithabid@gmail.com>
2021-11-20 20:37:31 -0600

    add recursive call

--- project-euler/fibonnaci.js
+++ project-euler/fibonnaci.js
@@ -4,7 +4,10 @@ function evenFibSum(n){
         return memo[n];
     if(n <= 1)
         return 1;
-     
+    let fibTerm = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+//         console.log(fibTerm);
+    if(fibTerm % 2 === 0)
+        sum += fibTerm; 
 }
 }
 
