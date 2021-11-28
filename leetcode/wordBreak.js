154562d79e343cf1c2d6d524e304602b27e3ac99
monk <munkithabid@gmail.com>
2021-11-27 21:27:08 -0600

    fix for loop boundey

--- leetcode/wordBreak.js
+++ leetcode/wordBreak.js
@@ -9,7 +9,16 @@ var wordBreak = function(s, wordDict) {
   console.log(wordDict)
   while(res !== '' && m < wordDict.length){
       let tw = wordDict[m];                                                   
-      
+      for(let i = m; i < wordDict.length; i++){
+          let cw = wordDict[i];
+          found = false;
+//             res = s;
+          for(j = 0; j < wordDict.length; j++){
+              let fw = wordDict[j];
+  //             if(found) break;
+             
+//                 if(found) break;
+          }
 //             m++;
 //             i = 0
   //         if(!found) return false;        
