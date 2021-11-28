2ed3f7e2183f1d62233ced1a672d962a8840c973
monk <munkithabid@gmail.com>
2021-11-27 21:25:58 -0600

    add outer while loop

--- leetcode/wordBreak.js
+++ leetcode/wordBreak.js
@@ -2,6 +2,21 @@ var wordBreak = function(s, wordDict) {
   let res = s;
   let found = false;
   let m = 0;
-  
+  wordDict = wordDict.filter( (w) => {
+      reg = new RegExp(`${w}`)
+      return s.search(reg) !== -1
+  })
+  console.log(wordDict)
+  while(res !== '' && m < wordDict.length){
+      let tw = wordDict[m];                                                   
+      
+//             m++;
+//             i = 0
+  //         if(!found) return false;        
+      }
+      res = s;
+      m++;
+  }
+  return found;
   
 };
