1c719d97051315f5c039f3c3e6f139056528a5b5
monk <munkithabid@gmail.com>
2021-11-27 21:29:24 -0600

    add replace() function

--- leetcode/wordBreak.js
+++ leetcode/wordBreak.js
@@ -1,41 +1,44 @@
 var wordBreak = function(s, wordDict) {
-  let res = s;
-  let found = false;
-  let m = 0;
-  wordDict = wordDict.filter( (w) => {
-      reg = new RegExp(`${w}`)
-      return s.search(reg) !== -1
-  })
-  console.log(wordDict)
-  while(res !== '' && m < wordDict.length){
-      let tw = wordDict[m];                                                   
-      for(let i = m; i < wordDict.length; i++){
-          let cw = wordDict[i];
-          found = false;
+    let res = s;
+    let found = false;
+    let m = 0;
+    wordDict = wordDict.filter( (w) => {
+        reg = new RegExp(`${w}`)
+        return s.search(reg) !== -1
+    })
+    console.log(wordDict)
+    while(res !== '' && m < wordDict.length){
+        let tw = wordDict[m];                                                   
+        for(let i = m; i < wordDict.length; i++){
+            let cw = wordDict[i];
+            found = false;
 //             res = s;
-          for(j = 0; j < wordDict.length; j++){
-              let fw = wordDict[j];
-  //             if(found) break;
-              for(let k = 0; k < wordDict.length; k++){
-                  let sw = wordDict[k];
-  //                 let pattern = new RegExp(`(?<=${cw}|${fw}|${sw}|^)${cw}(?=${cw}|${fw}|${sw}|$)`)
-                  let pattern2 = new RegExp(`(?<=${cw}|${fw}|${sw}|^)${cw}(?=${cw}|${fw}|${sw}|$)`, 'g')
-                  
-                  found = !!res.match(pattern2);
-                  if(res === '') 
-                      return true;
-                  
-              
-              }
+            for(j = 0; j < wordDict.length; j++){
+                let fw = wordDict[j];
+    //             if(found) break;
+                for(let k = 0; k < wordDict.length; k++){
+                    let sw = wordDict[k];
+    //                 let pattern = new RegExp(`(?<=${cw}|${fw}|${sw}|^)${cw}(?=${cw}|${fw}|${sw}|$)`)
+                    let pattern2 = new RegExp(`(?<=${cw}|${fw}|${sw}|^)${cw}(?=${cw}|${fw}|${sw}|$)`, 'g')
+                    
+                    found = !!res.match(pattern2);
+                    if(res === '') 
+                        return true;
+                    if(found) {
+                        res = res.replaceAll(pattern2, '');
+                                           
+                    }
+                
+                }
 //                 if(found) break;
-          }
+            }
 //             m++;
 //             i = 0
-  //         if(!found) return false;        
-      }
-      res = s;
-      m++;
-  }
-  return found;
-  
+    //         if(!found) return false;        
+        }
+        res = s;
+        m++;
+    }
+    return found;
+    
 };
