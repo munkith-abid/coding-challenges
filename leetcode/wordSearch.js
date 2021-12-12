f5631319531dadbaa70455b0cb6b06ac1e237037
monk <munkithabid@gmail.com>
2021-12-11 19:12:39 -0600

    create file challenge

---
+++ leetcode/wordSearch.js
@@ -0,0 +1,12 @@
+function getAdjacents(arr, {row, col, preRow, preCol}, prev) {
+
+  const above = {char: arr[ row - 1 ]?.[col], row: row - 1, col, preRow: row, preCol: col };
+  const below = {char: arr[ row + 1 ]?.[col], row: row + 1, col, preRow: row, preCol: col};
+  const before = {char: arr[row]?.[col - 1], row, col: col - 1, preRow: row, preCol: col};
+  const after = {char: arr[row]?.[col + 1], row, col: col + 1, preRow: row, preCol: col};
+  // const above = arr[row - 1]?.[col];
+  // const below = arr[row + 1]?.[col];
+  // const before = arr[row]?.[col - 1];
+  // const after = arr[row]?.[col + 1];
+
+  
