a22f8455a86e9d184bff6b48bfd53984b8cd002a
munkith-abid <munkithabid@gmail.com>
2021-12-24 19:59:32 -0600

    clean up the lot

--- amazon/assessment/shortestPath.js
+++ amazon/assessment/shortestPath.js
@@ -1,83 +1,49 @@
+//This is a BFS approach where two or more paths to target "race" each other. Whichever make it first wins!!!
 function shortestPath(grid) {
   const visited = {};
   const paths = [];
-  // let moves = 1;
-  const validSteps = [[0, 0, 0]];
-  const getValidSteps = (x, y, moves) => {
+  const validSteps = [[0, 0, 0]];//x coordinates, y coordinates, distance (steps) from start point.
+  const getValidSteps = (x, y, steps) => {
     let found = false;
     if(x > 0 && grid[y]?.[x - 1] === 9)
-      return ++moves;
+      return ++steps;
     else if(x > 0 && grid[y]?.[x - 1] === 1){
-      validSteps.push([x - 1, y, moves + 1]);
+      validSteps.push([x - 1, y, steps + 1]);
       found = true;
     }
 
     if(x < grid[0].length && grid[y]?.[x + 1] === 9)
-      return ++moves;
+      return ++steps;
     else if(x < grid[0].length && grid[y]?.[x + 1] === 1){
-      validSteps.push([x + 1, y, moves + 1]);
+      validSteps.push([x + 1, y, steps + 1]);
       found = true
     }
 
     if(y > 0 && grid[y - 1]?.[x] === 9)
-      return ++moves;
+      return ++steps;
     else if(y > 0 && grid[y - 1]?.[x] === 1){
-      validSteps.push([x, y - 1, moves + 1]);
+      validSteps.push([x, y - 1, steps + 1]);
       found = true
     }
 
     if(y < grid.length - 1 && grid[y + 1]?.[x] === 9)
-      return ++moves;
+      return ++steps;
     else if(y < grid.length - 1 && grid[y + 1]?.[x] === 1){
-      validSteps.push([x, y + 1, moves + 1]);
+      validSteps.push([x, y + 1, steps + 1]);
       found = true
     }
-    // if(found) moves++
-    grid[y][x] = "*";
+    grid[y][x] = "*";//mark current location as visited to avoid revisits.
     return null;
 
   }
   steps = (x, y) => {
-    const key = x + ',' + y;
-    // if(x >= grid[0].length || y >= grid.length){
-    //   return -1;
-    // }
-    // if(x >= 0 && y >= 0){
-    // if(key in visited){
-    //   return visited[key];
-    // }
     for(let i = 0; i < validSteps.length ; i++){
       const m = getValidSteps(...validSteps[i]);
       if(m)
         return m;
     }
-
-    // if (grid[y][x] === 1) {
-    //   grid[y][x] = 0;
-    //   ++moves;
-    //   visited[key] = moves;
-    //   while (1) {
-
-        // if(x > 0 && grid[y]?.[x - 1] !== 0) return steps(x - 1, y, moves);
-        // if(x < grid[0].length - 1 && grid[y]?.[x + 1] !== 0) return steps(x + 1, y, moves);
-        // if(y > 0 && grid[y - 1]?.[x] !== 0) return steps(x, y - 1, moves);
-        // if(y < grid.length - 1 && grid[y + 1]?.[x] !== 0) return steps(x, y + 1, moves);
-      // }
-      // const left = x + 1 < grid[0].length ? x + 1 : x;
-      // const right = x - 1 > 0 ? x - 1 : x;
-      // const top = y + 1 < grid.length ? y + 1 : y;
-      // const bottom = y - 1 > 0 ? y - 1 : y;
-      // return Math.min(steps(left, y, moves), steps(right, y, moves), steps(x, top, moves), steps(x, bottom, moves));
-    // }
-    // if (grid[y][x] === 9) {
-    //   paths.push(moves);
-    //   return;
-    // }
-    // }
-
     return -1;
   }
-  console.log(validSteps);
   return steps(0, 0)
 }
 const g1 = [[1, 1, 1, 0],
