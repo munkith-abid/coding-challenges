db845cd02b5ec1eae5b76920ea8a91e288cd0b23
munkith-abid <munkithabid@gmail.com>
2021-12-29 19:36:02 -0600

    update SGSI challenge #onlyIfUnique

--- SGSI/onlyIfUniqe.js
+++ SGSI/onlyIfUniqe.js
@@ -7,6 +7,5 @@ function onlyIfUnique(arr){
   const onlyUniqs = Object.entries(counts).filter(entry => entry[1] == 1);
   return onlyUniqs.length === 1 ? onlyUniqs[0][0] : [];
 }
-
 const arr1 = [4, 4, 5, 2, 2, 6, 6, 3, 3, 6];
 console.log(onlyIfUnique(arr1));
