9fb7cf6b99396b42dc01c618a3e214fabd150bf8
munkith-abid <munkithabid@gmail.com>
2021-12-29 20:13:44 -0600

    add left and right steps counters

--- vanguard/findClocest.js
+++ vanguard/findClocest.js
@@ -1,5 +1,6 @@
 const findClosest = (arr, startIndex, num) => {
   let rightSearch = leftSearch = startIndex;
+  let rightSteps = leftSteps = 0;
   while(true){
     if(rightSearch === arr.length ){
       rightSearch = 0;
@@ -8,15 +9,16 @@ const findClosest = (arr, startIndex, num) => {
       leftSearch = arr.length - 1;
     }
     if(arr[rightSearch] === num){
-      return rightSearch;
+      return rightSteps;
     }
     if(arr[leftSearch] === num){
-      return leftSearch;
+      return leftSteps;
     }
+    rightSteps = ++leftSteps;
     rightSearch++;
     leftSearch--;
   }
 }
 
-const arr = [2, 5, 6, 3, 7, 6, 1, 8, 9, 3];
-console.log(findClosest(arr, 5, 3));
+const arr = [ 2, 5, 6, 3, 7, 6, 1, 8, 9];
+console.log(findClosest(arr, 3, 3));
